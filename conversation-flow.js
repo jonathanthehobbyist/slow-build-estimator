a1351fbef76480c6210a325568093534e0da4882
jonathanpsimmons <jon@jonsimmons.co>
2025-07-06 20:08:38 -0500

    testing email content gating & email sending configuration

--- conversation-flow.js
+++ conversation-flow.js
@@ -66,7 +66,7 @@ const CONVERSATION_FLOW = {
   // ============== END INSPIRATION =================
 
   square_footage: {
-    question: `What's the approximate square footage of your ${sessionData}?`,
+    question: `What's the approximate square footage of your ?`,
     inputType: "number",
     validation: { min: 50, max: 2000, digitsOnly: true },
     lineItems: [], // Used for calculations
